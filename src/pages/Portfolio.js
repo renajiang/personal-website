bef4c500de95bdb7665b78b8df90e9bda7e3cdc2
rjiang6 <renajiang@RENAs-MacBook-Pro.local>
2021-07-21 15:52:33 -0400

    boutta change portfolio page layout

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,29 +1,34 @@
 import '../css/Portfolio.css';
 import Navbar from '../bars/Navbar';
 import Footer from '../bars/Footer';
+import {useState} from 'react';
 
 function Portfolio() {
 
-    // const [text, setText] = setState("");
+    const [text, setText] = useState('');
 
-    // function showText(text) {
-    //     console.log(text);
-    //     return (
-    //         // <h3>{text}</h3>
-    //     );
-    // }
-
-    //onMouseOver={()=>showText('FINE ARTS')}
+    function hover(t) {
+        if (text === '') {
+            setText(t);
+        } else {
+            setText('')
+        }
+        console.log(text);
+    }
 
     return (
         <div className='Portfolio'>
             <Navbar currentPage='portfolio'/>
-                <div class='Portfolio-categories'>
-                    <div class='Portfolio-category' id='Portfolio-fine' >
-                        {/* {text} */}
+                <div className='Portfolio-categories'>
+                    <div className='Portfolio-category' id='Portfolio-fine'
+                         onMouseEnter={()=>hover('FINE ARTS')}
+                         onMouseLeave={()=>hover('')}>
+                        <h3> {text} </h3>
                     </div>
-                    <div class='Portfolio-category' id='Portfolio-gd'>
-                        {/* {text} */}
+                    <div className='Portfolio-category' id='Portfolio-gd'
+                         onMouseEnter={()=>hover('GRAPHIC DESIGN')}
+                         onMouseLeave={()=>hover('')}>
+                        <h3> {text} </h3>
                     </div>
                 </div>
             <Footer/>
